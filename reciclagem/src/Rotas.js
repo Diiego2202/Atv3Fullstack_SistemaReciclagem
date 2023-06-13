2e6c03004afb884be9e58102c8f8d90787a6fa53
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2023-06-12 22:06:16 -0300

    casca projeto react

---
+++ reciclagem/src/Rotas.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./telas/login";
+import { Cadastro } from "./telas/Cadastro";
+import { Mapa } from "./telas/Mapa";
+
+export const Rotas = () => {
+
+    const rotas = createBrowserRouter([
+        {path: "/", element: <Login/>},
+        {path: "/cadastro/:userid", element: <Cadastro />},
+        {path: "/mapa", element: <Mapa />}
+    ]);
+
+    return <RouterProvider router={rotas}/>
+
+}
